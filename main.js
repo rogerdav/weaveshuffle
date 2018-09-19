c2fb571619a3424a3af115cb7fa90e7290eb53e8
Roger <roger.neil.davenport@gmail.com>
2018-09-19 11:07:58 -0700

    completed test

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 
 const shuffle = require('./weave');
 
-let n = 100;
-let times = shuffle(n);
-console.log(`when there are ${n} cards we need to shuffle ${times} times`);
+// let n = 100;
+const test = parseInt(process.argv[2]);
+console.log('value of test', test);
+let times = shuffle(test);
+console.log(`when there are ${test} cards we need to shuffle ${times} times`);
